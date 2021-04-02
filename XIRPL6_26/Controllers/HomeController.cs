1ab48248b022937173272086613fc879ab107ae4
nando <muhammad.rezki.ananda@gmail.com>
2021-04-02 19:25:06 +0700

    INITIAL COMMIT

---
+++ XIRPL6_26/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using XIRPL6_26.Security;
+
+namespace XIRPL6_26.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+        [AuthorizeRoles("Admin")]
+        public ActionResult AdminOnly()
+        {
+            return View();
+        }
+        public ActionResult UnAuthorized()
+        {
+            return View();
+        }
+    }
+}
